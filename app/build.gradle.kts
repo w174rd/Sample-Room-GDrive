e37c385467af4ee181cf267655f83370e739f08d
najib <najibbudinabdullah@gmail.com>
2025-02-07 20:19:16 +0700

    update minSdk to 23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.w174rd.sample_room_gdrive"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
